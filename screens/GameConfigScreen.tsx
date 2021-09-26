d601994e38cb8d26ab7940fdba08043cf293eb51
EyalAlfasiSkills <eyala@academy-skills.com>
2021-09-26 16:54:48 +0300

    .

--- screens/GameConfigScreen.tsx
+++ screens/GameConfigScreen.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Button, Platform, StyleSheet } from 'react-native';
-import { ITeam, TeamPreview } from '../components/TeamPreview';
 import { Text, View } from '../components/Themed';
 import { v4 as uuidv4 } from 'uuid';
 
